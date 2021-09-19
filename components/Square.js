388385210a88f0a0af0fb6d44587be010ad48f81
chapristi <70172942+chapristi@users.noreply.github.com>
2021-09-19 13:07:11 +0200

    Create Square.js

---
+++ components/Square.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {View,Text,StyleSheet} from 'react-native'
+class Square extends React.Component{
+  render(){
+    return(
+      <View style={[{backgroundColor : this.props.color} , styles.container]}>
+      
+      </View>
+    )
+  }
+}
+
+
+const styles =StyleSheet.create({
+  container : {height : 50 , width : 50}
+})
+export default Square
