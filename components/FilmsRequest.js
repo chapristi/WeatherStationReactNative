7e7fc97fb0ec55f9ef3b57b3a93d017cb9055c41
chapristi <70172942+chapristi@users.noreply.github.com>
2021-09-19 13:07:40 +0200

    Create FilmsRequest.js

---
+++ components/FilmsRequest.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { ActivityIndicator, FlatList, Text, View } from 'react-native';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: [],
+      isLoading: true
+    };
+  }
+
+  async getMovies() {
+    try {
+      const response = await fetch('https://reactnative.dev/movies.json');
+      const json = await response.json();
+      this.setState({ data: json.movies , isLoading: false  });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  componentDidMount() {
+    this.getMovies();
+  }
+
+  render() {
+    const { data, isLoading } = this.state;
+
+    return (
+      <View style={{ flex: 1, padding: 24 }}>
+        {isLoading ? <ActivityIndicator/> : (
+          <FlatList
+            data={data}
+            keyExtractor={({ id }, index) => id}
+            renderItem={({ item }) => (
+              <Text>{item.title}, {item.releaseYear}</Text>
+            )}
+          />
+        )}
+      </View>
+    );
+  }
+}
