a0b1cfcbd41b68219c0143280b7d91e0d754920a
chapristi <70172942+chapristi@users.noreply.github.com>
2021-09-19 21:44:22 +0200

    Update FilmsRequest.js

--- components/FilmsRequest.js
+++ components/FilmsRequest.js
@@ -1,45 +1,49 @@
 import React, { Component } from 'react';
-import { ActivityIndicator, FlatList, Text, View } from 'react-native';
+import {ActivityIndicator, FlatList, StyleSheet, Text, View} from 'react-native';
 
 export default class App extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      data: [],
-      isLoading: true
-    };
-  }
-
-  async getMovies() {
-    try {
-      const response = await fetch('https://reactnative.dev/movies.json');
-      const json = await response.json();
-      this.setState({ data: json.movies , isLoading: false  });
-    } catch (error) {
-      console.log(error);
+        this.state = {
+            data: [],
+            isLoading: true
+        };
     }
-  }
 
-  componentDidMount() {
-    this.getMovies();
-  }
+    async getMovies() {
+        try {
+            const response = await fetch('https://reactnative.dev/movies.json');
+            const json = await response.json();
+            this.setState({ data: json.movies , isLoading: false  });
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
-  render() {
-    const { data, isLoading } = this.state;
+    componentDidMount() {
+        this.getMovies();
+    }
 
-    return (
-      <View style={{ flex: 1, padding: 24 }}>
-        {isLoading ? <ActivityIndicator/> : (
-          <FlatList
-            data={data}
-            keyExtractor={({ id }, index) => id}
-            renderItem={({ item }) => (
-              <Text>{item.title}, {item.releaseYear}</Text>
-            )}
-          />
-        )}
-      </View>
-    );
-  }
+    render(style) {
+        const { data, isLoading } = this.state;
+
+        return (
+            <View style={styles.view}>
+                {isLoading ? <ActivityIndicator/> : (
+                    <FlatList
+                        data={data}
+                        keyExtractor={({ id }) => id}
+                        renderItem={({ item }) => (
+                            <Text>{item.title}, {item.releaseYear}</Text>
+                        )}
+                    />
+                )}
+            </View>
+        );
+    }
+
 }
+const styles =StyleSheet.create({
+
+})
