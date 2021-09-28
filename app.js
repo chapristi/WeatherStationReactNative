8037b1aee31b4f4c413eb28d5072c8f7b84b4073
chapristi <70172942+chapristi@users.noreply.github.com>
2021-09-28 20:41:49 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,60 @@
 import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, ActivityIndicator } from 'react-native';
 import Constants from 'expo-constants';
-
-import FilmRequest from './components/FilmRequest'
-
+import * as Location from 'expo-location';
+import axios from 'axios';
+import CurrentWeather from './components/currentWeather'
 export default function App() {
+  const API_URL = (lat, long) =>
+    `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=3780b59f3e313bbe20a3699001209872&lang=fr&units=metric`;
+
+  const [loading, setLoading] = React.useState(true);
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    const getCorrdinates = async () => {
+      const { status } = await Location.requestForegroundPermissionsAsync();
+      console.log(status);
+      if (status !== 'granted') {
+        return;
+      }
+      const userLocation = await Location.getCurrentPositionAsync();
+      getWeather(userLocation);
+    };
+    getCorrdinates();
+  }, []);
+  const getWeather = async (location) => {
+    try {
+      const response = await axios.get(
+        API_URL(location.coords.latitude, location.coords.longitude)
+      );
+      setData(response.data);
+      setLoading(false);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  if (loading) {
     return (
-
-        <View style = {styles.container}>
-            <FilmRequest />
-
-
-        </View>
+      <View style={styles.container}>
+        <ActivityIndicator style={{ color: 'blue' }} />
+      </View>
     );
+  }
+  return (
+    <View style={styles.container}>
+      <CurrentWeather data={data}/>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems : 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: 'grey',
+    padding: 8,
+  },
 });
